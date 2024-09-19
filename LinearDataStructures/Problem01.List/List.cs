caf22496d7b6e31adc5c8094d46da989476520d5
nikolaiandrov <nikolaiandrov@abv.bg>
2024-09-19 21:20:51 +0300

    RemoveAt implemented

--- LinearDataStructures/Problem01.List/List.cs
+++ LinearDataStructures/Problem01.List/List.cs
@@ -95,7 +95,15 @@
 
         public void RemoveAt(int index)
         {
-            throw new NotImplementedException();
+            this.ValidateIndex(index);
+
+            for (int i = index; i < this.Count - 1; i++)
+            {
+                this.items[i] = this.items[i + 1];
+            }
+
+            this.items[this.Count - 1] = default;
+            this.Count--;
         }
         public IEnumerator<T> GetEnumerator()
         {
