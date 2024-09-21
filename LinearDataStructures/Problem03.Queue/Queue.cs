743ab35aeaffc6ccfbb9543844e5827cad64e9e7
nikolaiandrov <nikolaiandrov@abv.bg>
2024-09-21 13:25:20 +0300

    initial create of the Queue

---
+++ LinearDataStructures/Problem03.Queue/Queue.cs
@@ -0,0 +1,62 @@
+﻿namespace Problem03.Queue
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+
+    public class Queue<T> : IAbstractQueue<T>
+    {
+        private class Node
+        {
+            public Node(T element, Node next)
+            {
+                this.Element = element;
+                this.Next = next;
+            }
+
+            public Node(T element)
+                : this(element, null)
+            {
+                this.Element = element;
+            }
+
+            public T Element { get; set; }
+
+            public Node Next { get; set; }
+        }
+
+        private Node head;
+
+        public int Count => throw new NotImplementedException();
+
+        public void Enqueue(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Dequeue()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Peek()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
