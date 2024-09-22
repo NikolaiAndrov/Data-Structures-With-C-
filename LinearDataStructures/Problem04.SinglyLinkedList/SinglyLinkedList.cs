5389b131edd4c4083d5969359a914ff032fa7c24
nikolaiandrov <nikolaiandrov@abv.bg>
2024-09-22 15:52:54 +0300

    SinglyLinkedList initial create

---
+++ LinearDataStructures/Problem04.SinglyLinkedList/SinglyLinkedList.cs
@@ -0,0 +1,70 @@
+﻿namespace Problem04.SinglyLinkedList
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+
+    public class SinglyLinkedList<T> : IAbstractLinkedList<T>
+    {
+        private class Node
+        {
+            public Node(T element, Node next)
+            {
+                this.Element = element;
+                this.Next = next;
+            }
+
+            public Node(T element)
+                : this(element, null)
+            {
+                this.Element = element;
+            }
+
+            public T Element { get; set; }
+
+            public Node Next { get; set; }
+        }
+
+        public int Count => throw new NotImplementedException();
+
+        public void AddFirst(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddLast(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetFirst()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetLast()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T RemoveFirst()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T RemoveLast()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
