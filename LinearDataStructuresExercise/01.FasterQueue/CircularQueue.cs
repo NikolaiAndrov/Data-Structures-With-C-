329cdbedecdeb2a83a35f1a99ea36b047ce7b2cc
nikolaiandrov <nikolaiandrov@abv.bg>
2024-09-28 13:06:40 +0300

    CircularQueue initial create

---
+++ LinearDataStructuresExercise/01.FasterQueue/CircularQueue.cs
@@ -0,0 +1,59 @@
+﻿namespace Problem01.CircularQueue
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+
+    public class CircularQueue<T> : IAbstractQueue<T>
+    {
+        private const int DefaultCapacity = 4;
+        private T[] items;
+
+        public CircularQueue()
+            : this(DefaultCapacity)
+        {
+        }
+
+        public CircularQueue(int capacity)
+        {
+            if (capacity < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(capacity));
+            }
+
+            this.items = new T[capacity];
+        }
+
+        public int Count { get; private set; }
+
+        public T Dequeue()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Enqueue(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Peek()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T[] ToArray()
+        {
+            throw new NotImplementedException();
+        }
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
