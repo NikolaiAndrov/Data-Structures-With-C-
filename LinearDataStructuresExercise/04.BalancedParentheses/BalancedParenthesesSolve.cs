3e742504c4362728c2defb54b6b3531ade0bbc17
nikolaiandrov <nikolaiandrov@abv.bg>
2024-09-30 12:30:53 +0300

    Balanced Parentheses solved

---
+++ LinearDataStructuresExercise/04.BalancedParentheses/BalancedParenthesesSolve.cs
@@ -0,0 +1,42 @@
+﻿namespace Problem04.BalancedParentheses
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class BalancedParenthesesSolve : ISolvable
+    {
+        public bool AreBalanced(string parentheses)
+        {
+            Stack<char> openParentheses = new Stack<char>();
+
+            foreach (char ch in parentheses)
+            {
+                if (ch == '(' || ch == '{' || ch == '[')
+                {
+                    openParentheses.Push(ch);
+                    continue;
+                }
+
+                char popped = default;
+
+                try
+                {
+                    popped = openParentheses.Pop();
+                }
+                catch (Exception)
+                {
+                    return false;
+                }
+
+                if (popped == '(' && ch != ')' || 
+                    popped == '{' && ch != '}' || 
+                    popped == '[' && ch != ']')
+                {
+                    return false;
+                }
+            }
+
+            return openParentheses.Count == 0;
+        }
+    }
+}
