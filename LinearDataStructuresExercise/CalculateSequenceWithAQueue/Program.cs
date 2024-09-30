46191803ad6c2d4bd1070b932acdd9f40967e5b7
nikolaiandrov <nikolaiandrov@abv.bg>
2024-09-30 14:22:07 +0300

    Calculate Sequence with a Queue

---
+++ LinearDataStructuresExercise/CalculateSequenceWithAQueue/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace CalculateSequenceWithAQueue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine()!);
+            Queue<int> nums = new Queue<int>();
+            nums.Enqueue(n);
+
+            for (int i = 1; i <= 50; i++)
+            {
+                nums.Enqueue(nums.Peek() + 1);
+                nums.Enqueue(2 * nums.Peek() + 1);
+                nums.Enqueue(nums.Peek() + 2);
+
+                Console.Write($"{nums.Dequeue()} ");
+            }
+        }
+    }
+}
