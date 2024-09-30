b810f0480627bc57ec831b2706c5a0a27f39a2af
nikolaiandrov <nikolaiandrov@abv.bg>
2024-09-30 13:36:39 +0300

    Reverse Numbers with a Stack

---
+++ LinearDataStructuresExercise/ReverseNumbersWithAStack/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ReverseNumbersWithAStack
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputNumbers = Console.ReadLine()!
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .AsEnumerable();
+
+            var reversedNumbers = new Stack<int>();
+
+            foreach (int number in inputNumbers)
+            {
+                reversedNumbers.Push(number);
+            }
+
+            Console.WriteLine(string.Join(" ", reversedNumbers));
+        }
+    }
+}
