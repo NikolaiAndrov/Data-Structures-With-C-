f5d8a633351e89dbf39071729fba1ac0d81fd470
nikolaiandrov <nikolaiandrov@abv.bg>
2024-10-28 21:42:08 +0200

    Initial skeleton

---
+++ LinearDataStructures/Demo/Program.cs
@@ -0,0 +1,17 @@
+﻿using Problem01.List;
+using Problem02.Stack;
+
+namespace Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = new Problem02.Stack.Stack<int>();
+
+            n.Push(1);
+            n.Push(2);
+            n.Push(3);
+        }
+    }
+}
---
+++ LinearDataStructuresExercise/Demo/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Demo
+{
+    using Problem01.CircularQueue;
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
