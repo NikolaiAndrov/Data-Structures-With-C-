226f568d345ac6913b0d4b860905dc4311ba9f75
nikolaiandrov <nikolaiandrov@abv.bg>
2024-11-18 23:26:52 +0200

    Insert implemented

--- Heaps-BST-Lab/02.BinarySearchTree/BinarySearchTree.cs
+++ Heaps-BST-Lab/02.BinarySearchTree/BinarySearchTree.cs
@@ -14,14 +14,14 @@
 
             public T Value { get; private set; }
 
-            public Node LeftChild { get; private set; }
+            public Node Left { get; set; }
 
-            public Node RightChild { get; private set; }
+            public Node Right { get; set; }
         }
 
         private Node root;
 
-        public BinarySearchTree() 
+        public BinarySearchTree()
         {
         }
 
@@ -42,14 +42,32 @@
                 return;
             }
 
-            this.EachInOrder(action, node.LeftChild);
+            this.EachInOrder(action, node.Left);
             action.Invoke(node.Value);
-            this.EachInOrder(action, node.RightChild);
+            this.EachInOrder(action, node.Right);
         }
 
-        public void Insert(T element)
+        public void Insert(T value)
         {
-            throw new NotImplementedException();
+            this.root = this.Insert(value, this.root);
+        }
+
+        private Node Insert(T value, Node node)
+        {
+            if (node == null)
+            {
+                node = new Node(value);
+            }
+            else if (value.CompareTo(node.Value) > 0)
+            {
+                node.Right = this.Insert(value, node.Right);
+            }
+            else if (value.CompareTo(node.Value) < 0)
+            {
+                node.Left = this.Insert(value, node.Left);
+            }
+
+            return node;
         }
 
         public IBinarySearchTree<T> Search(T element)
